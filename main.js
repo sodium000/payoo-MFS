479381b0ce8e86fb9a242da5ba105f272c3920a1
Raisul Islam Tonmoy <sodiumrdx@gmail.com>
2025-09-14 01:02:41 +0600

    cash out fanctionality adde

--- main.js
+++ main.js
@@ -9,7 +9,7 @@
         const Bank = document.getElementById("bank").value
         console.log(Bank)
         
-        const BankaccountNumber = parseInt(document.getElementById("Bank-accnumber").value) 
+        const BankaccountNumber = (document.getElementById("Bank-accnumber").value) 
 
         const AddAmount = parseInt(document.getElementById("add-amound").value)
         const PinNumber = parseInt(document.getElementById("pin-number").value)
@@ -27,6 +27,35 @@
         }
 
         const totalAmount = AddAmount + Taka
+
+        document.getElementById("taka").innerText = totalAmount
+
+        
+    });
+
+    const withdraw = document.getElementById("withdraw-money")
+
+    withdraw.addEventListener("click", function(e){
+        e.preventDefault();
+        // console.log("click add money button")
+        const agentBankaccountNumber = (document.getElementById("agent-Bank").value) 
+
+        const withdrawAmount = parseInt(document.getElementById("withdraw-amound").value)
+        const PinNumber = parseInt(document.getElementById("withdraw-pin-number").value)
+
+        const Taka =  parseInt (document.getElementById("taka").innerText)
+
+        if (agentBankaccountNumber.length !== 11 ) {
+            alert("Provide Valid account Number")
+            return;
+        }
+
+        if (PinNumber!==validpin) {
+            alert("please enter valid pin number")
+            return;
+        }
+
+        const totalAmount = Taka - withdrawAmount;
 
         document.getElementById("taka").innerText = totalAmount
 
